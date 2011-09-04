d7d161eeadd4bc89718dea02546136e2fc38d9e0
Valentin Rusu <kde@rusu.info>
2011-09-04 17:25:12 +0200

    The client API moved to kdelibs
    See kdelibs branch named ksecretsservice
    Location : kdeui/ksecretsservice

--- syncprotocol.cpp
+++ syncprotocol.cpp
@@ -21,8 +21,8 @@
 #include "syncprotocol.h"
 #include "ksecretsynccfg.h"
 #include "synclogger.h"
-#include "../daemon/frontend/secret/adaptors/dbustypes.h"
-#include "../client/ksecretsservicecollection.h"
+#include <ksecretsservicedbustypes.h>
+#include <ksecretsservicecollection.h>
 #include "service_interface.h"
 #include "session_interface.h"
 
